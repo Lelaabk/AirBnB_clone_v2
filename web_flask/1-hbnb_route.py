2c82643ab37e56b1be62ed1482c8fc2738460691
Lelaabk <lolabkari@gmail.com>
2024-01-27 07:51:02 -0800

    Hbnb Route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Display 'HBNB' """
+    return 'HBNB'
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
